fdce54983ecc9ee8637bee5cf9a027ffc030bf79
Mateusz Witek <45691135+WitekM95@users.noreply.github.com>
2019-07-05 07:39:27 +0200

    dodanie spiring sec

--- client/src/test/com/springboot/client/SystemPrintTest.java
+++ client/src/test/com/springboot/client/SystemPrintTest.java
@@ -4,24 +4,26 @@ import org.assertj.core.api.WithAssertions;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
+
 import static org.junit.jupiter.api.Assertions.*;
 
 
 class SystemPrintTest extends SystemPrint implements WithAssertions {
 
 
-    private Object dataFromPShell;
 
-    @Test
-    void dataFromPShell1() {
+
+//    @Test
+//    void dataFromPShell1() {
+//
 //        assertTrue(true);
-//        assertThat().isNotEqualTo();
-
-
-
-    }
-
-    @Test
-    void addPrintPostRest1() {
-    }
+//        assertThat().isEqualTo();
+//
+//
+//
+//    }
+//
+//    @Test
+//    void addPrintPostRest1() {
+//    }
 }
