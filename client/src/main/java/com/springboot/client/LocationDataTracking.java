ea6c578611cfd11d974e1ffe55230db990a3088a
Mateusz Witek <45691135+WitekM95@users.noreply.github.com>
2019-07-08 14:38:16 +0200

    dodanie spiring sec

--- client/src/main/java/com/springboot/client/LocationDataTracking.java
+++ client/src/main/java/com/springboot/client/LocationDataTracking.java
@@ -1,38 +1,57 @@
 package com.springboot.client;
 
 import com.profesorfalken.jpowershell.PowerShell;
+
 import com.profesorfalken.jpowershell.PowerShellResponse;
+
+import java.util.StringTokenizer;
 
 
 public class LocationDataTracking extends SystemPrint {
 
+    public static String location;
+
     public static void main(String[] args) {
-        getDataLocation();
-        cutDataOut();
 
-        return;
-    }
-
-    private static void cutDataOut() {
-
-
+        getLocalizationFromPowerShell();
+        cutDataFromPowerShell();
 
     }
 
-    public static void getDataLocation() {
-        try (PowerShell powerShell = PowerShell.openSession()) {
-            {
 
-                PowerShellResponse response;
-                response = powerShell.executeCommand("get-wmiobject -class win32_networkadapterconfiguration | Select-Object ipaddress");
-                String location = response.getCommandOutput();
-                System.out.println(location);
 
-            }
+    public static void getLocalizationFromPowerShell() {
+        // wywołanie polecenia powershell do adresu ip, przekierowanie do zmiennej i zamknięcie sesji
+        PowerShell locationOpen = PowerShell.openSession();
+        {
+            PowerShellResponse response;
+            response = locationOpen.executeCommand("get-wmiobject -class win32_networkadapterconfiguration | Select-Object ipaddress");
+            location = response.getCommandOutput();
+           // System.out.println(location); //TODO wyciecic
+            locationOpen.close();
+        }
+
+    }
+    public static void cutDataFromPowerShell() {
+
+        StringTokenizer stringTokenizer = new StringTokenizer(location, "{}");
+        while(stringTokenizer.hasMoreTokens())
+        {
+            String klucz = stringTokenizer.nextToken();
+            String wartosc = stringTokenizer.nextToken();
+            location = wartosc;
+            System.out.println(location); //TODO wyciać
         }
 
 
     }
+
+
+
+
+
+
+
 
 
 }
---
+++ client/src/test/com/springboot/client/LocationDataTrackingTest.java
@@ -0,0 +1,12 @@
+package com.springboot.client;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LocationDataTrackingTest extends SystemPrint {
+
+
+
+
+}
